41b40928c6ccd9b462973c15ccad859b539a41d4
Dong Kyun <laancer4@gmail.com>
2023-01-03 11:13:06 +0900

    좋아요 부분 save / delete 이용해서 오르락내리락 하도록(댓글부분)

--- src/main/java/com/sparta/homework/service/LikesService.java
+++ src/main/java/com/sparta/homework/service/LikesService.java
@@ -1,8 +1,10 @@
 package com.sparta.homework.service;
 
 import com.sparta.homework.dto.CommentsResponseDto;
+import com.sparta.homework.dto.MemoResponseDto;
 import com.sparta.homework.entity.Comments;
 import com.sparta.homework.entity.Likes;
+import com.sparta.homework.entity.Memo;
 import com.sparta.homework.entity.User;
 import com.sparta.homework.repository.CommentsRepository;
 import com.sparta.homework.repository.LikesRepository;
@@ -19,7 +21,9 @@ public class LikesService {
     private final LikesRepository likesRepository;
     private final CommentsRepository commentsRepository;
     private final MemoRepository memoRepository;
+
 // addOrDeleteComment
+
     @Transactional
     public CommentsResponseDto likeComment(Long id, User user) {
         Comments comments = commentsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 코멘트가 없습니다"));
@@ -28,20 +32,8 @@ public class LikesService {
             Likes likes = likesRepository.saveAndFlush(new Likes(comments,user));
             comments.getLikes().add(likes);
             // 얘가 문제일까? comments에는 남고 likesRepository에서는 delete 를 쏜건가?
-        }
-        return CommentsResponseDto.from(comments);
-    }
-
-
-    // 지금 형태는 좋지는 않다. 눌렀을 때 like 다시누르면 delete의 유추가 어렵다.
-    // 차라리 넣는거랑 빼는걸 따로 만들자.
-    @Transactional
-    public CommentsResponseDto deleteCommentLikes(Long id, User user){
-        Comments comments = commentsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("X"));
-        if (likesRepository.findByUserId(user.getId()).isPresent()) {
+        } else if (likesRepository.findByUserId(user.getId()).isPresent()) {
             likesRepository.deleteByUserId(user.getId());
-        } else {
-            throw new IllegalArgumentException("실패");
         }
         return CommentsResponseDto.from(comments);
     }
