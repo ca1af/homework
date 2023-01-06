f36b796e31661250a994fa447302762b3f799617
Dong Kyun <laancer4@gmail.com>
2023-01-06 11:41:05 +0900

    test코드 작성 연습

--- src/main/java/com/sparta/homework/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/homework/dto/MemoRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.homework.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class MemoRequestDto {
     private String username;
     private String title;
--- src/test/java/com/sparta/homework/service/LikesCommentServiceTest.java
+++ src/test/java/com/sparta/homework/service/LikesCommentServiceTest.java
@@ -1,34 +1,55 @@
 package com.sparta.homework.service;
 
+import com.sparta.homework.dto.CommentsRequestDto;
+import com.sparta.homework.dto.MemoRequestDto;
+import com.sparta.homework.entity.Comments;
+import com.sparta.homework.entity.Memo;
+import com.sparta.homework.entity.User;
+import com.sparta.homework.entity.UserRoleEnum;
+import com.sparta.homework.repository.CommentsRepository;
 import com.sparta.homework.repository.LikesCommentRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@Component
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // 클래스마다 쓰레드 인스턴스 생성하겠다.
 class LikesCommentServiceTest {
-    @Autowired
-    private final LikesService likesService;
-    @Autowired
-    private final LikesCommentRepository likesCommentRepository;
-
-    LikesCommentServiceTest(LikesService likesService, LikesCommentRepository likesCommentRepository) {
-        this.likesService = likesService;
-        this.likesCommentRepository = likesCommentRepository;
-    }
-
+    @InjectMocks
+    private LikesService likesService;
+    @Mock // SpyBean도 한번 찾ㅇ보자
+    private LikesCommentRepository likesCommentRepository;
+    @Mock
+    private CommentsRepository commentsRepository;
+    @BeforeAll
+    void init() {
+        MockitoAnnotations.openMocks(this); 	}
     @Test
     void like() {
-//        //given
-//        User user = new User("user1", "12345!", UserRoleEnum.USER);
-//        MemoRequestDto memoRequestDto = new MemoRequestDto("name", "title", "contents");
-//        Memo memo = new Memo(memoRequestDto, user);
-//        CommentsRequestDto commentsRequestDto = new CommentsRequestDto("comment");
-//        Comments comments = new Comments(commentsRequestDto, memo, user.getUsername());
-//        //when
-//        likesService.likeComment(comments.getId());
+        //given
+        User user = new User("user1", "12345!", UserRoleEnum.USER);
+        MemoRequestDto memoRequestDto = new MemoRequestDto("name", "title", "contents");
+        Memo memo = new Memo(memoRequestDto, user);
+        CommentsRequestDto commentsRequestDto = new CommentsRequestDto("comment");
+        Comments comments = new Comments(commentsRequestDto, memo, user.getUsername());
+        when(commentsRepository.findById(comments.getId())).thenReturn(Optional.of(comments));
+
+        //when
+        likesService.likeComment(comments.getId(),user);
 
         //then
-//        assertNotNull(likesCommentRepository.findByUserId(comments.getId()));
+
+//        then(likesCommentRepository).should(times(1)).save(message);
+        verify(likesCommentRepository, times(1)).countLikesCommentByCommentsId(comments.getId());
     }
 }
