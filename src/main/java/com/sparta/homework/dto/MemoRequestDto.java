9d0752dae968f6078e9b5de4717d9890a72009c2
Dong Kyun <laancer4@gmail.com>
2023-01-06 15:14:51 +0900

    .

--- src/main/java/com/sparta/homework/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/homework/dto/MemoRequestDto.java
@@ -1,10 +1,8 @@
 package com.sparta.homework.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class MemoRequestDto {
     private String username;
     private String title;
